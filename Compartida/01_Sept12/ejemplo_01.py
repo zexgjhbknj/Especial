102f2a1e4e28a07090ec4452b8717a649aedcdce
faiverht <faiverht@gmail.com>
2019-09-13 15:06:41 -0500

    Iniciando el repo

---
+++ Compartida/01_Sept12/ejemplo_01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+# comentarios una linea
+# para texto "hola" o 'hola'
+# comentarios extensos ''' mucho texto ''' o """  mucho texto """
+print('hola'," buenos días")
+
+#help('numpy') # información acerca del paquete numpy
+
+import numpy as np  # se importa el paquete para poder utilizarlo mediante un alias (np, yo me lo invento)
+
+print('El valor de la cte llamada FPE_UNDERFLOW es:',np.FPE_UNDERFLOW)
+
+print('otras cosas')
